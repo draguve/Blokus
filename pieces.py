71c28a0bba4375cb7038139cdf3bd72fd88aa1ec
Draguve <ritwikpatil1998@gmail.com>
2023-10-25 03:21:27 +0530

    Added all the pentominos

--- pieces.py
+++ pieces.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# TODO: Need to able to flip all options
 class Piece:
     def __init__(self, boundingBoxSize=(1, 1)):
         self.possible_points = None
@@ -107,6 +108,126 @@ class TetrominoE(Piece):
         self.shape[1, :] = True
         self.shape[:, 0] = True
         self.possible_points = np.array([(0, 0), (0, 1), (1, 2), (2, 2), (3, 1), (3, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoF(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(3, 3))
+        self.shape[:, 1] = True
+        self.shape[1, 0:2] = True
+        self.shape[0, 1:3] = True
+        self.possible_points = np.array([(1, 0), (0, 1), (0, 3), (1, 3), (3, 2), (3, 1), (2, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoI(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(5, 1))
+        self.shape[:, :] = True
+        self.possible_points = np.array([(0, 0), (5, 0), (0, 1), (5, 1)])
+        self.different90 = True
+
+
+class PentominoL(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(4, 2))
+        self.shape[:, 0] = True
+        self.shape[0, :] = True
+        self.possible_points = np.array([(0, 0), (0, 2), (1, 2), (4, 1), (4, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoN(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(5, 2))
+        self.shape[0:2, 0] = True
+        self.shape[1:5, 1] = True
+        self.possible_points = np.array([(0, 0), (0, 1), (1, 2), (5, 2), (5, 1), (2, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoP(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(3, 2))
+        self.shape[0:2, 0:2] = True
+        self.shape[2, 0] = True
+        self.possible_points = np.array([(0, 0), (0, 2), (2, 2), (3, 1), (3, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoT(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(3, 3))
+        self.shape[:, 1] = True
+        self.shape[0, :] = True
+        self.possible_points = np.array([(0, 0), (0, 3), (1, 3), (3, 2), (3, 1), (1, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoU(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(3, 2))
+        self.shape[:, 1] = True
+        self.shape[0, :] = True
+        self.shape[2, :] = True
+        self.possible_points = np.array([(0, 0), (0, 2), (3, 2), (3, 0), (2, 0), (1, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoV(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(3, 3))
+        self.shape[:, 0] = True
+        self.shape[2, :] = True
+        self.possible_points = np.array([(0, 0), (0, 1), (2, 3), (3, 3), (3, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoW(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(3, 3))
+        self.shape[0, 0:2] = True
+        self.shape[1, 1:3] = True
+        self.shape[2, 2] = True
+        self.possible_points = np.array([(0, 0), (0, 2), (1, 3), (3, 3), (3, 2), (2, 1), (1, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoX(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(3, 3))
+        self.shape[1, :] = True
+        self.shape[:, 1] = True
+        self.possible_points = np.array([(1, 0), (0, 1), (0, 2), (1, 3), (2, 3), (3, 2), (3, 1), (2, 0)])
+
+
+class PentominoY(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(4, 2))
+        self.shape[2, :] = True
+        self.shape[:, 0] = True
+        self.possible_points = np.array([(0, 0), (0, 1), (2, 2), (3, 2), (4, 1), (4, 0)])
+        self.different90 = True
+        self.different180 = True
+
+
+class PentominoZ(Piece):
+    def __init__(self):
+        super().__init__(boundingBoxSize=(3, 3))
+        self.shape[:, 1] = True
+        self.shape[0, 0:2] = True
+        self.shape[2, 1:3] = True
+        self.possible_points = np.array([(0, 0), (0, 2), (2, 3), (3, 3), (3, 1), (1, 0)])
         self.different90 = True
         self.different180 = True
 
