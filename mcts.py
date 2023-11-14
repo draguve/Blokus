44232cc410ca30d72f75ac132082fa5727cdbd37
Draguve <ritwikpatil1998@gmail.com>
2023-11-14 01:39:09 -0800

    Kindof working mcts, needs ucb cache,batching and need to confirm if it works correctly

--- mcts.py
+++ mcts.py
@@ -1,10 +1,23 @@
 import math
 
 import numpy as np
-from .games.blokus import Game
+# from .games.blokus import Game
 from numba import njit
 
 
+class Model:
+    def __init__(self):
+        pass
+
+    def infer(self, obs, o=None):
+        value = np.random.rand(1)
+        reward = np.random.rand(1)
+        policy_logits = np.random.rand(780)
+        hidden_state = np.random.rand(32)
+        return value, reward, policy_logits, hidden_state
+
+
+# TODO Can we cache this
 # @njit(parallel=True)
 def get_children_ucb(
         parent_node_id,
@@ -39,9 +52,10 @@ def get_children_ucb(
 
 def get_children_value(children_node_ids, id_to_value_sum, id_to_visit_count):
     values = np.zeros(children_node_ids.shape[0])
-    indexes_where_positive = (id_to_visit_count[children_node_ids] > 0).nonzero()
-    ids_where_positive = children_node_ids[indexes_where_positive]
-    values[indexes_where_positive] = id_to_value_sum[ids_where_positive] / id_to_visit_count[ids_where_positive]
+    indexes_where_positive = (id_to_visit_count[children_node_ids] > 0).nonzero()[0]
+    if len(indexes_where_positive)>0:
+        ids_where_positive = children_node_ids[indexes_where_positive]
+        values[indexes_where_positive] = id_to_value_sum[ids_where_positive] / id_to_visit_count[ids_where_positive]
     return values
 
 
@@ -51,10 +65,41 @@ def get_node_value(node_id, id_to_value_sum, id_to_visit_count):
     return id_to_value_sum[node_id] / id_to_visit_count[node_id]
 
 
+def depth_to_player_turn(depth):
+    return (depth // 2) % 2
+
+
+def get_nodes_to_expand(
+        current_depth,
+        batch_size,
+        root_node_id,
+        max_depth,
+        node_id_expanded,
+):
+    virtual_to_play = np.full(batch_size, depth_to_player_turn(current_depth))
+    node_ids = np.full(batch_size, root_node_id)
+    search_paths = np.zeros((batch_size, max_depth), dtype=np.uint16)
+    search_length = np.ones(batch_size, dtype=int)
+    actions = np.full(batch_size, -1, dtype=np.uint16)
+    for i in range(batch_size):
+        while node_id_expanded[node_ids[i]]:
+            action, node = self.select_child(node_ids[i])  # need to add a function to ignore already selected nodes
+            search_paths[i][search_length[i]] = node
+            search_length[i] += 1
+
+            virtual_to_play = depth_to_player_turn(search_length[i] + current_depth - 1)
+
+
 class MCTS:
-    def __init__(self, max_number_of_nodes, size_of_action_space, hidden_state_size, pb_c_base, pb_c_init,
-                 discount, max_num_simulations):
-        self.node_index = 0
+    def __init__(self,
+                 max_number_of_nodes,  # this prob can be calculated or be kept dynamic
+                 size_of_action_space,
+                 hidden_state_size,
+                 pb_c_base=19652,
+                 pb_c_init=1.25,
+                 discount=0.997,
+                 max_num_simulations=800):
+        self.num_of_nodes = 1  # we will always have the root node provided by the run function
         self.node_id_to_visit_count = np.zeros(max_number_of_nodes, dtype=int)
         self.node_id_to_player = np.full(max_number_of_nodes, -1, dtype=np.uint8)
         self.node_id_to_value_sum = np.zeros(max_number_of_nodes, dtype=float)
@@ -76,22 +121,38 @@ class MCTS:
         self.max_num_simulations = max_num_simulations
         self.max_tree_depth = max_num_simulations
 
+    def reinit(self):
+        self.num_of_nodes = 1
+        self.node_id_to_visit_count[:] = 0
+        self.node_id_to_player[:] = -1
+        self.node_id_to_value_sum[:] = 0
+        self.node_id_to_prior[:] = 0
+        self.node_id_expanded[:] = 0
+        self.node_id_to_children_length[:] = -1
+        self.node_id_to_parent_action[-1] = 0
+        self.node_id_to_children_node_ids[:] = 0
+        self.node_id_to_hidden_state[:] = 0
+        self.node_id_to_reward[:] = 0
+        self.min_value = float("inf")
+        self.max_value = -float("inf")
+
     def current_number_of_nodes(self):
-        return self.node_index + 1
+        return self.num_of_nodes
 
     def expand_node(self, node_id, valid_actions: np.ndarray, to_play: int, reward: float, policy_logits: np.ndarray,
                     hidden_state: np.ndarray):
         self.node_id_to_player[node_id] = to_play
-        self.node_id_to_reward[node_id] = reward
+        self.node_id_to_reward[node_id] = reward[0]  # TODO Fix this later on
         self.node_id_to_hidden_state[node_id] = hidden_state
 
         num_new_nodes = valid_actions.shape[0]
         self.node_id_to_children_length[node_id] = num_new_nodes
-        new_node_ids = np.arange(0, num_new_nodes, dtype=int) + self.node_index
+        new_node_ids = np.arange(0, num_new_nodes, dtype=int) + self.num_of_nodes
         self.node_id_to_children_node_ids[node_id, 0:num_new_nodes] = new_node_ids
         self.node_id_to_prior[new_node_ids] = policy_logits[valid_actions]
         self.node_id_to_parent_action[new_node_ids] = valid_actions
-        self.node_index += num_new_nodes
+        self.node_id_expanded[node_id] = True
+        self.num_of_nodes += num_new_nodes
 
     def add_noise_to_node(self, node_id, alpha, frac):
         children_ids = self.node_id_to_children_node_ids[node_id]
@@ -99,13 +160,13 @@ class MCTS:
         self.node_id_to_prior[children_ids] = self.node_id_to_prior[children_ids] * (1 - frac) + noise * frac
 
     def select_child(self, node):
-        children_ids = self.node_id_to_children_node_ids[0:self.node_id_to_children_length[node]]
+        children_ids = self.node_id_to_children_node_ids[node, 0:self.node_id_to_children_length[node]]
         ucb_values = get_children_ucb(
             node, children_ids, self.node_id_to_visit_count, self.node_id_to_prior,
             self.node_id_to_value_sum, self.node_id_to_reward, self.max_value, self.min_value,
             self.pb_c_base, self.pb_c_init, self.discount
         )
-        max_ids = children_ids[np.argmax(ucb_values)]
+        max_ids = np.flatnonzero(ucb_values == np.max(ucb_values))
         selected_node = np.random.choice(max_ids)
         return self.node_id_to_parent_action[selected_node], selected_node
 
@@ -113,41 +174,61 @@ class MCTS:
         value = value
         for i in reversed(range(search_path.shape[0])):
             node_id = search_path[i]
-            self.node_id_to_value_sum[node_id] += value
+            self.node_id_to_value_sum[node_id] += value[0] # TODO Fix this later
             self.node_id_to_visit_count[node_id] += 1
             value = self.node_id_to_reward[node_id] + self.discount * get_node_value(node_id, self.node_id_to_value_sum,
                                                                                      self.node_id_to_visit_count)
             self.min_value = min(self.min_value, value)
             self.max_value = max(self.max_value, value)
 
-    def run(self, model, initial_observation, legal_actions, to_play):
+    def run(self, model, initial_observation, legal_actions, current_depth):
+        to_play = depth_to_player_turn(current_depth)
         root_node_id = 0
         root_value, reward, policy_logits, hidden_state = model.infer(initial_observation)
         self.expand_node(root_node_id, legal_actions, to_play, reward, policy_logits, hidden_state)
-
-        max_tree_depth = 0
+        # max_tree_depth = 0
         search_path = np.zeros(self.max_tree_depth, dtype=np.uint16)
-        search_path_max = 0
+
+        search_path[0] = root_node_id
+        search_path_length = 1
+
+        self.back_propagate(search_path[0:search_path_length], root_value)
+
         for _ in range(self.max_num_simulations):
             virtual_to_play = to_play
             node = root_node_id
             search_path[0] = root_node_id
-            search_path_max = 1
-            action=-1
+            search_path_length = 1
+            action = -1
 
             while self.node_id_expanded[node]:
                 action, node = self.select_child(node)
-                search_path[search_path_max] = node
-                search_path_max += 1
+                search_path[search_path_length] = node
+                search_path_length += 1
 
-                virtual_to_play = #how to fix this, dont know yet
+                virtual_to_play = depth_to_player_turn(search_path_length + current_depth - 1)
 
-            parent_id = search_path[search_path_max-2]
-            value, reward, policy_logits, hidden_state = model.recurrent_inference(
+            parent_id = search_path[search_path_length - 2]
+            value, reward, policy_logits, hidden_state = model.infer(
                 self.node_id_to_hidden_state[parent_id],
                 action
             )
             if to_play != virtual_to_play:
                 reward *= -1
-            self.expand_node(node,self.action_space,virtual_to_play,reward,policy_logits,hidden_state)
-            self.back_propagate(search_path[0:search_path_max],value)
+            self.expand_node(node, self.action_space, virtual_to_play, reward, policy_logits, hidden_state)
+            self.back_propagate(search_path[0:search_path_length], value)
+
+
+def main():
+    model = Model()
+    mcts = MCTS(
+        30000,
+        780,
+        32,
+    )
+    legal_actions = np.arange(0, 780, dtype=np.uint16)
+    mcts.run(model, None, legal_actions, 0)
+
+
+if __name__ == '__main__':
+    main()
